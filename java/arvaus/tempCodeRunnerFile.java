14887950c902b5f5b4d80ceb992a4b103d9240d0
Esa Hanikka <esso@phobos.localdomain>
2018-03-23 18:59:34 +0200

    ...

--- java/arvaus/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-            // if (luku < 1) {
-            //     System.out.println("Epäkelpo luku!");
-            //     continue;
-            // }
-            
-            // if (luku > 10) {
-            //     System.out.println("Epäkelpo luku!");
-            //     continue;
-            // }
