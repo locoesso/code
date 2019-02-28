f24321c2aef60ca3bf9f2a8d4fa98b9749235e45
Esa Hanikka <esa.hanikka@gmail.com>
2019-02-28 21:53:36 +0200

    Unity

---
+++ csharp/MyApppp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApppp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello me!");
+            Console.WriteLine($"The current time is {DateTime.Now}");
+        }
+    }
+}
