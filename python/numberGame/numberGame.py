8b86e4690554aa500e34dd8655f3b7f5294c261e
LocoEsso <xxx@xxx.com>
2022-05-22 17:22:57 +0300

    xxxxxx

---
+++ python/numberGame/numberGame.py
@@ -0,0 +1,38 @@
+import random
+
+num = random.randint(0, 10)
+#print(f'''
+#---------------------------
+#Correct answer: {num}
+#---------------------------
+#''')
+
+print(f'''
+Number is something between 1-10
+
+You have 4 attempts
+''')
+
+attempt = 4
+msg = ''
+while attempt > 0:
+
+    user_input = int(input('Enter Number: '))
+    if user_input == num:
+        msg = 'You Won!'
+        break
+    elif user_input > num:
+        print(f'{user_input} is greater.\nRemaining attempts: {attempt}.')
+        attempt -= 1
+
+
+    elif user_input < num:
+        print(f'{user_input} is smaller.\nRemaining attempts: {attempt}.')
+        attempt -= 1
+
+    else:
+        print('Something went wrong!')
+        break
+
+print(msg)
+56
