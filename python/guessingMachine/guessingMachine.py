b30cd479312fc4eb2ca863c4c394bd6bcc94ff76
LocoEsso <esa.hanikka@gmail.com>
2022-05-23 12:39:34 +0300

    dfidsofdhi

--- python/guessingMachine/guessingMachine.py
+++ python/guessingMachine/guessingMachine.py
@@ -9,7 +9,8 @@ while guess != number:
     guess = random.randint(1, 1000)
     if guess == number:
         print('')
-        print(f'Correct number was {number} and it took {attempt} attempts to guess it.')
+        # print(f'Correct number was {number} and it took {attempt} attempts to guess it.')
+        msg = ('Correct number was {number} and it took {attempt} attempts to guess it.')
         # print(f'{attempt}.\t {guess}')
         break
     elif guess != number:
@@ -18,3 +19,5 @@ while guess != number:
     else:
         print('Something went wrong!')
         break
+
+print(msg)
