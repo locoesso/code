ff8e496209fb60c3f8f01141b4b9afc4cdde4d66
LocoEsso <esa.hanikka@gmail.com>
2022-05-31 20:39:55 +0300

    sadsdxzczcx

--- java/guessingMachine/guessingMachine.java
+++ java/guessingMachine/guessingMachine.java
@@ -3,20 +3,20 @@ public class guessingMachine {
 
     public static void main(String[] args) {
 
-        int arvattava = (int) (1 + Math.random() * 100);
+        int arvattava = (int) (1 + Math.random() * 1000);
         int arvauksia = 0;
-        int luku = 0;
+        int arvaus = 0;
         
         // System.out.println(arvattava);
         
         while (true) {
-            if (arvattava == luku) {
+            if (arvattava == arvaus) {
                 break;
             }
             
-            luku = (int) (1 + Math.random() * 100);
+            arvaus = (int) (1 + Math.random() * 1000);
             arvauksia++;
-            System.out.println(arvauksia + ". " + luku);
+            System.out.println(arvauksia + ". " + arvaus);
         }
         System.out.println("Arvauksia " + arvauksia);
     }
